79c01d9375b44784b3396df30b6202af99a11e6f
micangl <142919381+micangl@users.noreply.github.com>
2024-02-16 13:36:18 +0100

    Disable changing tags when scrolling on root.
    By default, scrolling with mouse wheel (buttons 4 and 5) changed tags.
    This is quite annoying when using gaps, as scrolling when the cursor in
    on them will change tags.

--- rc.lua
+++ rc.lua
@@ -210,8 +210,8 @@ end)
 
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    --awful.button({ }, 4, awful.tag.viewnext),
+    --awful.button({ }, 5, awful.tag.viewprev)
 ))
 -- }}}
 
